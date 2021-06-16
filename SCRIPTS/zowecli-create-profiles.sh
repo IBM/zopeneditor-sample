093d7a8a4afa57642f8cfdae873fc2758b937b09
Peter Haumer <4391934+phaumer@users.noreply.github.com>
2021-06-16 15:46:11 -0700

    Updates for DBB and Debug

--- SCRIPTS/zowecli-create-profiles.sh
+++ SCRIPTS/zowecli-create-profiles.sh
@@ -17,9 +17,13 @@ SSHPORT=22                             # ssh port
 SSHPROFILE=ssh-profile                 # ssh profileName
 
 echo "Creating RSE profile and setting as default..."
+set -x
 zowe profiles create rse-profile ${RSEPROFILE} --host ${HOSTNAME} --port ${RSEPORT} --user ${TSOUSER} --pass ${PASSWORD} --bp rseapi --protocol https --reject-unauthorized false
 zowe profiles set rse ${RSEPROFILE}
+set +x
 
 echo "Creating SSH profile and setting as default..."
+set -x
 zowe profiles create ssh-profile ${SSHPROFILE} --host ${HOSTNAME} --user ${TSOUSER} --pass ${PASSWORD} --port ${SSHPORT}
 zowe profiles set ssh ${SSHPROFILE}
+set +x
