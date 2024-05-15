f0502df0aa8563f5c00cb5eef92874d79f6f8808
Peter Haumer <4391934+phaumer@users.noreply.github.com>
2024-05-15 14:43:04 -0700

    Updated for v4.2.0 with HLASM macro samples

---
+++ ASMMAC/MPUT.asm
@@ -0,0 +1,15 @@
+****************************************************************
+* LICENSED MATERIALS - PROPERTY OF IBM
+* "RESTRICTED MATERIALS OF IBM"
+* (C) COPYRIGHT IBM CORPORATION 2024. ALL RIGHTS RESERVED
+* US GOVERNMENT USERS RESTRICTED RIGHTS - USE, DUPLICATION,
+* OR DISCLOSURE RESTRICTED BY GSA ADP SCHEDULE
+* CONTRACT WITH IBM CORPORATION
+****************************************************************
+         MACRO
+&MPUT    MPUT  &OUTFILE,&OUTAREA
+&MPUT    LA    1,&OUTFILE
+         LA    0,&OUTAREA
+         L     15,48(0,1)
+         BALR  14,15
+         MEND
