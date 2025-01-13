55c69fbf32ca9882df566f7045c4536d4d13d67b
Peter Haumer <4391934+phaumer@users.noreply.github.com>
2025-01-12 16:59:10 -0800

    Added devcontainer specification for Codespaces

---
+++ .devcontainer/installZowe.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+###############################################################################
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2024. All Rights Reserved.
+#
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+###############################################################################
+
+npm install -g @zowe/cli@zowe-v3-lts
+zowe plugins install @ibm/rse-api-for-zowe-cli
